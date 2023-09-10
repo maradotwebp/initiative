4bef80fffbb1d1ebb288f3b1a153bd48f111c95d
froehlicha <froehlich.alexander@protonmail.com>
2023-09-10 14:15:38 +0200

    make css responsive

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -6,7 +6,7 @@
 
 <style scoped>
 div {
-  border: solid 3px #41403E;
+  border: solid 3px #41403Ecc;
   border-top-left-radius: 255px 15px;
   border-top-right-radius: 15px 225px;
   border-bottom-right-radius: 225px 15px;
--- src/components/CharacterCreator.vue
+++ src/components/CharacterCreator.vue
@@ -9,7 +9,7 @@ const x = defineProps<{
 </script>
 
 <template>
-  <div>
+  <div class="wrapper">
     <Box class="character">
       <Input class="name-input" maxlength="20" v-model="character.name" />
       <div class="hp">
@@ -26,18 +26,34 @@ const x = defineProps<{
 </template>
 
 <style scoped>
-.character {
+.wrapper {
   display: flex;
+  flex-direction: column;
+  align-items: end;
+}
+
+.character {
+  align-self: stretch;
+
+  display: flex;
+  flex-direction: column;
   gap: 1em;
   justify-content: space-between;
+  align-items: end;
   background: #f9f9f9;
   padding: 0.5em 0.5em;
+}
+
+.name-input {
+  align-self: stretch;
 }
 
 .actions {
   border-width: 0 2px 2px 2px;
   width: fit-content;
-  padding: 0.25em 0.25em;
+  padding: 0.25em;
+
+  margin: 0 0.6em;
 }
 
 .hp {
@@ -46,5 +62,12 @@ const x = defineProps<{
 
 .hp-input {
   width: 5ch;
+}
+
+@media screen and (min-width: 720px) {
+  .character {
+    flex-direction: row;
+    align-items: stretch;
+  }
 }
 </style>
